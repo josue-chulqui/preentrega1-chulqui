7d982903263774849e9c5633da8b38afd2393830
josue chulqui <josue.c1095@gmail.com>
2023-11-04 22:30:16 -0300

    arreglos en las funciones

---
+++ js/index.js
@@ -0,0 +1,33 @@
+
+function pedirPresupuesto(codigo) {// codigo: traerá el id del producto
+    switch(codigo) { 
+        case 1:
+            alert("El sedan cuesta us$ 5000")
+            precio = 5500
+            break
+        case 2:
+            alert("La utilitaria cuesta us$ 7500")
+            precio = 4000
+            break
+        case 3:
+            alert("El minibus cuesta us$ 10000")
+            break
+        case 4:
+            alert("El Camión cuesta us$ 70000")
+            break
+        default:
+            alert("😥😑😖😔😏😅 No entendimos tu pedido.")
+    }
+}
+
+function pedirPresupuesto() {
+    // let preguntar = true
+    let codigoPrenda = prompt("Ingresa el código numérico de la prenda a consultar:")
+
+    if (codigoPrecio !== '' && codigoPrecio !== null) {
+        pedirPresupuesto(parseInt(codigoPrecio))
+        
+    } else {
+        console.warn("Ingresa un código de prenda válido.")
+    }
+}
